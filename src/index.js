335c40896dcd2ea6ba820a6e5fe44a63a73d5502
rgguriev <60221719+rgguriev@users.noreply.github.com>
2021-03-15 18:33:58 +0100

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array === 0 || array === undefined || array.length === 0) {
+    return 0;
+  }
+  return Math.min(...array);
+  
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array === 0 || array === undefined || array.length === 0) {
+    return 0;
+  }
+  return Math.max(...array);
+  
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array === 0 || array === undefined || array.length === 0) {
+    return 0;
+  }
+  let sum = 0;
+  let len = array.length;
+  for (let i = 0; i < len; i++) {
+      sum += array[i];
+  }
+  let avg = sum / len;
+  return avg;
 }

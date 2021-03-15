6efaab30bc31ecf1a91df5aebbd3fe2d932143f8
rgguriev <60221719+rgguriev@users.noreply.github.com>
2021-03-16 00:31:21 +0100

    feat: check task

--- src/index.js
+++ src/index.js
@@ -1,29 +1,29 @@
-
-exports.min = function min (array) {
-  if (array === 0 || array === undefined || array.length === 0) {
-    return 0;
-  }
-  return Math.min(...array);
-  
-}
-
-exports.max = function max (array) {
-  if (array === 0 || array === undefined || array.length === 0) {
-    return 0;
-  }
-  return Math.max(...array);
-  
-}
-
-exports.avg = function avg (array) {
-  if (array === 0 || array === undefined || array.length === 0) {
+exports.avg = function avg(array) {
+  if (!Array.isArray(array) || array === 0 || array === undefined || array.length === 0) {
     return 0;
   }
   let sum = 0;
   let len = array.length;
   for (let i = 0; i < len; i++) {
-      sum += array[i];
+    sum += array[i];
   }
-  let avg = sum / len;
-  return avg;
+  return sum / len;
 }
+
+exports.min = function min (array) {
+  if (!Array.isArray(array) || array === 0 || array === undefined || array.length === 0) {
+    return 0;
+  }
+  return Math.min.apply(Array, array);
+  
+}
+
+exports.max = function max (array) {
+  if (!Array.isArray(array) || array === 0 || array === undefined || array.length === 0) {
+    return 0;
+  }
+  return Math.max.apply(Array, array);
+  
+}
+
+
